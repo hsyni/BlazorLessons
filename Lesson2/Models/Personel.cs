9464cd508c4f97320ea845efb49e13395f1fe6b3
Huseyin <h.ozgur.kazan@gmail.com>
2024-07-11 13:57:22 +0300

    TableFilter Added.

--- Lesson2/Models/Personel.cs
+++ Lesson2/Models/Personel.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Ad { get; set; }
         public string Soyad { get; set; }
-        public string DogumTarihi { get; set; }
+        public DateTime DogumTarihi { get; set; }
         public string Meslegi { get; set; }
         public int Yasi { get; set; }
     }
